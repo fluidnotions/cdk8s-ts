63312fc4b0d75a3d76a28fc475384af97e7f5f8b
Justin <justinr@cybicom.com>
2023-07-24 19:13:03 +0200

    wip

--- lib/web-service.ts
+++ lib/web-service.ts
@@ -1,20 +1,15 @@
 import { Construct } from 'constructs';
 import { Names } from 'cdk8s';
-import { KubeDeployment, KubeService, IntOrString } from '../imports/k8s';
+import { KubeDeployment, KubeService, IntOrString, Quantity, KubePersistentVolumeClaim, EnvVar, KubeDeploymentProps } from '../imports/k8s';
+import { set } from 'lodash';
 
 export interface WebServiceProps {
+  readonly namespace: string;
   /**
    * The Docker image to use for this service.
    */
   readonly image: string;
-
-  /**
-   * Number of replicas.
-   *
-   * @default 1
-   */
-  readonly replicas?: number;
-
+  // readonly replicas: number;
   /**
    * External port.
    *
@@ -28,6 +23,15 @@ export interface WebServiceProps {
    * @default 8080
    */
   readonly containerPort?: number;
+
+  readonly containerEnv?: EnvVar[];
+
+  readonly pvc?: {
+    pvcName: string;
+    containerMountPath: string;
+    storageClassName?: string;
+  }
+
 }
 
 export class WebService extends Construct {
@@ -37,19 +41,25 @@ export class WebService extends Construct {
     const port = props.port || 80;
     const containerPort = props.containerPort || 8080;
     const label = { app: Names.toDnsLabel(this) };
-    const replicas = props.replicas ?? 1;
+    const replicas = 1;
+    const namespace = props.namespace;
+    const storageClassName = props.pvc?.storageClassName ?? "microk8s-hostpath"
+    const containerEnv =  props.containerEnv ?? []
+    const namePrefix = id
 
     new KubeService(this, 'service', {
+      metadata: { namespace, name: `${namePrefix}-service` },
       spec: {
-        type: 'LoadBalancer',
+        type: 'ClusterIP',
         ports: [ { port, targetPort: IntOrString.fromNumber(containerPort) } ],
         selector: label
       }
     });
-
-    new KubeDeployment(this, 'deployment', {
+    const deployment: KubeDeploymentProps = {
+      metadata: { namespace, name: `${namePrefix}-deployment` },
       spec: {
         replicas,
+       
         selector: {
           matchLabels: label
         },
@@ -60,12 +70,37 @@ export class WebService extends Construct {
               {
                 name: 'app',
                 image: props.image,
-                ports: [ { containerPort } ]
+                ports: [ { containerPort } ],
+                env: containerEnv
               }
             ]
           }
         }
       }
-    });
+    }
+    if(props.pvc){
+      new KubePersistentVolumeClaim(this, 'pvc', {
+        metadata: {
+          name: props.pvc.pvcName!,
+          namespace: namespace,
+          annotations: {associatedDeployment: `${namePrefix}-deployment`}
+        },
+        spec: {
+          accessModes: ['ReadWrite'],
+          storageClassName: storageClassName,
+          resources: { requests: { storage:  Quantity.fromString('1Gi')} }
+        },
+      });
+      set(deployment, 'spec.template.spec.volumes', [
+        {
+          name: 'data',
+          persistentVolumeClaim: {
+            claimName: props.pvc.pvcName
+          }
+        }
+      ])
+      set(deployment, 'spec.template.spec.containers[0].volumeMounts[0].mountPath', props.pvc.containerMountPath)
+    }
+    new KubeDeployment(this, 'deployment', deployment);
   }
 }
--- main.ts
+++ main.ts
@@ -1,14 +1,31 @@
-import * as kplus from 'cdk8s-plus-26';
-import * as cdk8s from 'cdk8s';
+import { Construct } from 'constructs';
+import { App, Chart, ChartProps } from 'cdk8s';
+import { WebService, WebServiceProps } from './lib/web-service';
 
-const app = new cdk8s.App();
-const chart = new cdk8s.Chart(app, 'Chart');
 
-new kplus.Deployment(chart, 'Deployment', {
-  replicas: 1,
-  containers: [{
-    image: 'ubuntu',
-  }],
-});
+export class MyChart extends Chart {
+  constructor(scope: Construct, id: string, props: ChartProps = { }) {
+    super(scope, id, props);
 
+    const camWebServiceProps: WebServiceProps = {
+       namespace: 'support',
+       image: 'camunda/camunda-bpm-platform:7.20.0-SNAPSHOT',
+       containerEnv: [{
+          name: 'DB_URL',
+          value: 'jdbc:h2:/camunda/data'
+       }],
+       pvc: {
+          pvcName: 'camunda-data',
+          containerMountPath: '/camunda/data'
+       }
+    }
+
+    new WebService(this, 'camunda-bpm-platform_7_20', camWebServiceProps);
+   
+
+  }
+}
+
+const app = new App();
+new MyChart(app, 'support');
 app.synth();
